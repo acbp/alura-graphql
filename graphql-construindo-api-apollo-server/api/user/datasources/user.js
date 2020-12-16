b8b032621df05b20635a06125b94edbbaed60337
Alexandre Calil Blasizza Paravani <paravanimobile@gmail.com>
2020-12-16 07:53:40 -0300

    aulas 3 e 4

--- graphql-construindo-api-apollo-server/api/user/datasources/user.js
+++ graphql-construindo-api-apollo-server/api/user/datasources/user.js
@@ -4,15 +4,58 @@ const URL = `http://0.0.0.0:3000`
 class UsersRepository extends RESTDataSource {
   constructor(){
     super();
-    this.baseURL = URL;
+    this.baseURL = URL
   }
 
   async getUsers(){
-    return this.get(`/users`);
+    const users = await this.get(`/users`)
+    return users.map( async u => await this.mapUserRole(u) )
   }
 
   async getUserById(id){
-    return this.get(`/users/${id}`);
+    const user = await this.get(`/users/${id}`)
+    return await this.mapUserRole(user)
+  }
+
+  async getRoleById(id){
+    return this.get(`/roles/${id}`)
+  }
+
+  async postUser(user){
+    const length = (await this.get(`/users`)).length+1
+    // todo: validação role
+    const roleId = (await this.get(`/roles?type=${user.role}`))[0].id
+    return this.post(
+      `users`,
+      {
+        ...user,
+        id: length,
+        role:roleId
+      }
+    )
+  }
+
+  async putUser(user){
+    const roleId = (await this.get(`/roles?type=${user.role}`))[0].id
+    return this.put(
+      `users/${user.id}`,
+      {
+        ...user,
+        role:roleId
+      }
+    )
+  }
+
+  async deleteUser(id){
+    await this.delete(`users/${id}`)
+    return id
+  }
+
+  async mapUserRole(user){
+    return {
+      ...user,
+      role: await this.getRoleById(user.role)
+    }
   }
 } 
 
--- graphql-construindo-api-apollo-server/api/user/resolvers/userResolvers.js
+++ graphql-construindo-api-apollo-server/api/user/resolvers/userResolvers.js
@@ -1,8 +1,13 @@
 const userResolvers = {
   Query: {
-    users: (parent, args, { dataSources }, info) => dataSources.usersAPI.getUsers(),
-    user: (parent, { id }, { dataSources } )=> dataSources.usersAPI.getUserById(id),
+    users: (parent, args, { dataSources }, info ) => dataSources.usersAPI.getUsers(),
+    user: (parent, { id }, { dataSources }, info )=> dataSources.usersAPI.getUserById(id),
   },
+  Mutation: {
+    postUser:  (parent, user, { dataSources }, info )=> dataSources.usersAPI.postUser(user),
+    putUser:  (parent, user, { dataSources }, info )=> dataSources.usersAPI.putUser(user),
+    deleteUser:  (parent, {id}, { dataSources }, info )=> dataSources.usersAPI.deleteUser(id),
+  }
 };
 
 module.exports = userResolvers;
